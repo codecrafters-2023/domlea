e6bffe5349b28430ed3364baa4433520209bca11
codecrafters <codecrafters@gurmaanitservices.com>
2025-03-30 03:56:25 +0530

    changes

--- frontend/src/pages/DomainDetails/index.js
+++ frontend/src/pages/DomainDetails/index.js
@@ -88,11 +88,7 @@ const DomainDetails = () => {
     if (!domain) return
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh" }}>
         <div className='loader'>
-            {/* <div class="ui-loader loader-blk">
-            <svg viewBox="22 22 44 44" class="multiColor-loader">
-                <circle cx="44" cy="44" r="20.2" fill="none" stroke-width="3.6" class="loader-circle loader-circle-animation"></circle>
-            </svg>
-        </div> */}
+
         </div>
     </div>;
 
