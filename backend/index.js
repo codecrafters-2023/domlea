ea2257589723fee43e4eb78a41e88ba8d9cb4c2f
codecrafters-2023 <145508415+codecrafters-2023@users.noreply.github.com>
2025-04-02 20:54:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Database
+connectDB();
+
 // Middleware
 app.use(express.json());
 const allowedOrigins = [
@@ -19,12 +22,17 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-    origin: '*',
-    credentials: true
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, origin);
+        } else {
+            callback(new Error('CORS not allowed'));
+        }
+    },
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
-
-// Database
-connectDB();
 
 app.get('/test-cors', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -38,12 +46,26 @@ app.use((req, res, next) => {
     res.setHeader('Connection', 'keep-alive');
     next();
 });
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 // Routes
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 app.use('/api/auth', authRoutes);
-app.use('/api/domains', domainRoutes);
+app.use('/api/domains', (req, res, next) => {
+    console.log('Incoming Request to /api/domains');
+    console.log('Method:', req.method);
+    console.log('Headers:', req.headers);
+    console.log('Body:', req.body);
+    next();
+}, domainRoutes);
 app.use('/api/users', userRoutes);
 
 app.use((err, req, res, next) => {
